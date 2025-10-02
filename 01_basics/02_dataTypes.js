4ba27849b31f45d739bf64f40f6d115c94061ce2
Divyanshu Rajput <divyanshu2907tkd@gmail.com>
2025-10-02 23:31:48 +0000

    data types.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3)    // code readability should be high
+console.log("Hitesh")
+
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false  
+let state; // undefined
+let nullVar = null   // it means value is empty
+
+
+// 7 data types in js
+
+
+// number => 2 to power 53
+// bigint => larger than 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => standalone value
+// symbol => unique identifier
+
+
+// object
+
+console.log(typeof "Hitesh") // string
+console.log(typeof null) // object (bug)
+console.log(typeof undefined) // undefined
